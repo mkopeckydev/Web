adf3762977568dd9f53d93ddf96d13ea2adcb84f
Michal Kopecký <kpc@email.cz>
2025-11-10 19:20:18 +0100

    Links refaktoring

--- src/Kopec.Web/Model/IconListItem.cs
+++ src/Kopec.Web/Model/IconListItem.cs
@@ -10,32 +10,7 @@ namespace Kopec.Web.Model
 
         public string RowsLabel {  get; set; } = String.Empty;
         public List<string> Rows { get; set; } = new List<string>();
-        
-        public string Link { get; set; } = String.Empty;
-        public string LinkLabel { get; set; } = String.Empty;
-        public string LinkPrefix { get; set; } = "Web";
-
-        public string Link2 { get; set; } = String.Empty;
-        public string Link2Label { get; set; } = String.Empty;
-        public string Link2Prefix { get; set; } = "Web";
-
-        [JsonIgnore]
-        public bool HasLink
-        {
-            get
-            {
-                return !String.IsNullOrEmpty(Link) && !String.IsNullOrEmpty(LinkLabel) && !String.IsNullOrEmpty(LinkPrefix);
-            }
-        }
-
-        [JsonIgnore]
-        public bool HasLink2
-        {
-            get
-            {
-                return !String.IsNullOrEmpty(Link2) && !String.IsNullOrEmpty(Link2Label) && !String.IsNullOrEmpty(Link2Prefix);
-            }
-        }
+        public List<IconListLink> Links { get; set; } = new List<IconListLink>();
 
         [JsonIgnore]
         public bool HasRowsLabel
---
+++ src/Kopec.Web/Model/IconListLink.cs
@@ -0,0 +1,9 @@
+﻿namespace Kopec.Web.Model
+{
+    public class IconListLink
+    {
+        public string Url { get; set; } = String.Empty;
+        public string UrlLabel { get; set; } = String.Empty;
+        public string Label { get; set; } = String.Empty;
+    }
+}
